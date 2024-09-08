822f71f43abd5895b5dcd2e63b12f7e94ee3d8f0
Ana Westphalen <ana.westphalen0712@gmail.com>
2024-09-08 18:13:23 -0300

    feat: create Log model

--- server/db/schema.rb
+++ server/db/schema.rb
@@ -10,9 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_09_08_152211) do
+ActiveRecord::Schema[7.0].define(version: 2024_09_08_205536) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "logs", force: :cascade do |t|
+    t.text "content"
+    t.bigint "created_by_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["created_by_id"], name: "index_logs_on_created_by_id"
+  end
 
   create_table "pets", force: :cascade do |t|
     t.string "img"
@@ -67,6 +75,7 @@ ActiveRecord::Schema[7.0].define(version: 2024_09_08_152211) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "logs", "users", column: "created_by_id"
   add_foreign_key "user_shelters", "shelters"
   add_foreign_key "user_shelters", "users"
 end
