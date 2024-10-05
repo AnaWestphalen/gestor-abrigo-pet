7fcf0f49394b9ec936930eb00095cdc847455ca8
Ana Westphalen <ana.westphalen0712@gmail.com>
2024-10-04 23:54:47 -0300

    removes validation to create pet record and changes pet image storage to base64

--- server/app/controllers/pets_controller.rb
+++ server/app/controllers/pets_controller.rb
@@ -2,7 +2,6 @@ class PetsController < ApplicationController
   before_action :set_pet, only: [:show, :edit, :update, :destroy]
   before_action :authorize_pet, only: [:edit, :update, :destroy]
   before_action :authenticate_user!, except: [:search]
-  before_action :authorize_shelter_user, only: [:new, :create]
 
   def authorize_pet
     authorize @pet
@@ -11,7 +10,7 @@ class PetsController < ApplicationController
   # GET /api/shelters/:shelter_id/pets
   def index
     @shelter = Shelter.find(params[:shelter_id])
-    @pets = policy_scope(Pet).where(shelter: @shelter).includes(img_attachment: :blob)
+    @pets = policy_scope(Pet).where(shelter: @shelter)
     render json: @pets.as_json(
       only: [:id, :name, :specie, :color, :size, :age, :description, :found_in, :received_at, :left_at],
       methods: [:img_url]
@@ -42,7 +41,7 @@ class PetsController < ApplicationController
 
     if @pet.save
       if params[:img].present?
-        @pet.img.attach(params[:img])
+        @pet.img = params[:img]
       end
 
       render json: {
@@ -64,8 +63,7 @@ class PetsController < ApplicationController
 
     if @pet.update(pet_params)
       if params[:img].present?
-        @pet.img.purge if @pet.img.attached?
-        @pet.img.attach(params[:img])
+        @pet.img = params[:img]
       end
 
       render json: {
@@ -120,13 +118,6 @@ class PetsController < ApplicationController
 
   def pet_params
     params.require(:pet).permit(:name, :specie, :color, :size, :age, :description, :found_in, :tutor_name, :tutor_contact, :img, :received_at, :left_at)
-  end
-
-  def authorize_shelter_user
-    @shelter = Shelter.find(params[:shelter_id])
-    unless @shelter.users.include?(current_user)
-      render json: { error: 'Você não tem permissão para acessar este abrigo.' }, status: :forbidden
-    end
   end
 
   def pet_with_img_url(pet)
